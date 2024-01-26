18fa9b70ed551641c18c6196b327e88acc3437c9
zxcv9675 <zxcv9675@naver.com>
2024-01-26 21:06:38 +0900

    Add java file

---
+++ Account.java
@@ -0,0 +1,17 @@
+package org.swstudy.java;
+
+public class Account {
+
+	String accountNumber;
+	String name;
+	int money;
+	
+	void save(String accountNumber,String name,int money)
+	{
+		this.accountNumber=accountNumber;
+		this.name=name;
+		this.money=money;
+	}
+	
+	
+}
---
+++ BankApplication.java
@@ -0,0 +1,35 @@
+package org.swstudy.java;
+
+import java.util.Scanner;
+
+public class BankApplication {
+
+	public static void main(String[] args) {
+		
+		String[] Account = new String[100];
+		
+		Scanner scanner = new Scanner(System.in);
+		Account account = new Account();
+		
+		System.out.println("------------------------------------------");
+		System.out.println("1.계좌생성 . 2.계좌목록 . 3.예금 . 4.출금 . 5.종료");
+		System.out.println("------------------------------------------");
+		
+		System.out.print("선택> ");
+		String select = scanner.nextLine();
+		
+		if(select=="1")
+		{
+			System.out.print("계좌번호: ");
+			String accountNumber = scanner.nextLine();
+			System.out.print("계좌주: ");
+			String name = scanner.nextLine();
+			System.out.print("계좌주: ");
+			String money = scanner.nextLine();
+		}
+		
+		
+		
+	}
+
+}
